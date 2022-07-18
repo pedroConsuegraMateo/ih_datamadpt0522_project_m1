9800b8c7e2bf408587f7a140679a6e30e103d0c8
PedroConsuegra <pedroconsuegramateo@gmail.com>
2022-07-18 18:49:21 +0100

    fuzzywuzzyokei

--- main.py
+++ main.py
@@ -5,13 +5,23 @@ from modules import main_table as mt
 
 import argparse
 from fuzzywuzzy import process
+import folium
 
 def argument_parser():
+    
     parser = argparse.ArgumentParser(description= 'Obtención de las paradas más cercanas a cada centro médico' )
     help_message ='Elige una opción. Option 1: "all" devuelve una tabla con todos los datos centro-parada. Option 2: "NOMBRE_CENTRO" devuelve el registro centro-parada indicado' 
     parser.add_argument('-f', '--function', help=help_message, type=str)
     args = parser.parse_args()
+    
     return args
+
+def fuzzywuzzy_implementation(main_table,input):
+            
+    nombre_centros = [main_table['Place of Interest'][i] for i in range(len(main_table))]
+    aprox = process.extractOne(nombre, nombre_centros)
+    
+    return aprox
 
 if __name__ == '__main__':
     
@@ -28,19 +38,18 @@ if __name__ == '__main__':
     if argument_parser().function == 'all':
         main_table.to_csv('./data/main_table.csv')
     else:
-        nombre = argument_parser().function
         
-        aproximacion = ''
-        precision = -1
-        for i in range(len(main_table)):
-            if precision < process.extractOne(str(nombre), main_table['Place of Interest'][i])[1]:
-                aproximacion = process.extractOne(str(nombre), main_table['Place of Interest'][i])[0]
-                precision = process.extractOne(str(nombre), main_table['Place of Interest'][i])[1]
-                 
+        nombre = argument_parser().function  
+        aprox = fuzzywuzzy_implementation(main_table,nombre)
         
-        main_table = main_table[main_table['Place of Interest'] == aproximacion]
-        aproximacion = aproximacion.replace(' ', '_')
-        main_table.to_csv(f'./data/{aproximacion}.csv')
+        nombre = aprox[0]
+        if aprox[1] < 90:
+            print('No se ha encontrado ninguna coincidencia.')
+        else:
+            
+            main_table = main_table[main_table['Place of Interest'] == nombre]      
+            nombre = nombre.replace(' ', '_')
+            main_table.to_csv(f'./data/{nombre}.csv')
         
-    print('Finish!! :)')
+    print('Finished!')
     
